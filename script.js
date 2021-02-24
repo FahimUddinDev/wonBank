90337565906d913cd8e6f34e97814b96f48a55e9
Fcode9 <50692937+Fcode9@users.noreply.github.com>
2021-02-25 01:34:52 +0600

    final

--- script.js
+++ script.js
@@ -9,36 +9,51 @@ submitBtn.addEventListener('click', function(){
 
 const dipositBtn = document.getElementById('dipositBtn');
 dipositBtn.addEventListener('click', function(){
-    const dipositInput = document.getElementById('depositInput').value;
-    const dipositAmount = parseFloat(dipositInput);
-    const displayOldDiposit = document.getElementById('currentDiposit').innerText;
-    const oldDipositAmount = parseFloat(displayOldDiposit);
-    const totalDiposit = dipositAmount+oldDipositAmount;
-    document.getElementById('currentDiposit').innerText = totalDiposit;
+
+    const Amount = inputNumber('depositInput');
+
     document.getElementById('depositInput').value="";
-    const balance = document.getElementById('balanceAmount').innerText;
-    const balanceAmount = parseFloat(balance);
-    const balanceAfterDiposit = balanceAmount + dipositAmount;
-    document.getElementById('balanceAmount').innerText= balanceAfterDiposit;
+
+    dipositMainBalance('currentDiposit', Amount);
+    dipositMainBalance('balanceAmount', Amount);
+
+
 });
+function inputNumber(id){
+    const dipositInput = document.getElementById(id).value;
+    const number = parseFloat(dipositInput);
+    return number;
+}
+function dipositMainBalance(id,Amount){
+    const balance = document.getElementById(id).innerText;
+    const balanceAmount = parseFloat(balance);
+    const total = balanceAmount + Amount;
+    document.getElementById(id).innerText= total;
+}
 
 const withdrawBtn = document.getElementById('withdrawBtn');
 withdrawBtn.addEventListener('click', function(){
-    const withdrawInput = document.getElementById('withdrawInput').value;
-    const withdrawAmount = parseFloat(withdrawInput);
+    // const withdrawInput = document.getElementById('withdrawInput').value;
+    // const withdrawAmount = parseFloat(withdrawInput);
+    const withdrawAmount = inputNumber('withdrawInput');
+    // dipositMainBalance('currentWithdraw', withdrawAmount);
+    // dipositMainBalance('balanceAmount', -1*withdrawAmount);
     const displayOldWithdraw = document.getElementById('currentWithdraw').innerText;
     const oldWithdrawAmount = parseFloat(displayOldWithdraw);
     const totalWithdraw = withdrawAmount + oldWithdrawAmount;
-
     const balance = document.getElementById('balanceAmount').innerText;
     const balanceAmount = parseFloat(balance);
     const balanceAfterWithdraw = balanceAmount - withdrawAmount;
     if  ( balanceAmount>=withdrawAmount){
-        document.getElementById('balanceAmount').innerText= balanceAfterWithdraw;
-        document.getElementById('currentWithdraw').innerText = totalWithdraw;
-        document.getElementById('withdrawInput').value="";
+    document.getElementById('balanceAmount').innerText= balanceAfterWithdraw;
+    document.getElementById('currentWithdraw').innerText = totalWithdraw;
+    document.getElementById('withdrawInput').value="";
     } else{
         alert('Withdraw amount is geterthen Your balance');
     };
-
+    function amount (id, output){
+        const withdraw = document.getElementById(id).output;
+        const withdrawAmount = parseFloat(withdraw);
+        return withdrawAmount;
+    };
 });
